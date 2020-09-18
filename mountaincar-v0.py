a6bc3034766dede615e4c9c8aaa6b0684769f79a
Isaac Holston <isaac.holston@outlook.com>
2020-09-18 10:58:26 -0600

    Update mountaincar-v0.py

--- mountaincar-v0.py
+++ mountaincar-v0.py
@@ -1,31 +1,34 @@
-import gym
+import gym, math
 import numpy as np
 gym.logger.set_level(40) # Might need this to remove logger warning https://stackoverflow.com/questions/60149105/userwarning-warn-box-bound-precision-lowered-by-casting-to-float32
 
 class mountaincar_driver:
-    def __init__(self, alpha, gamma, epsilon):
-        print("------------------------------------")
+    def __init__(self, alpha, gamma, epsilon, min_epsilon):
+        print("------------------------------")
         print("Initializing Training...")
         print("Learning Rate: {}".format(alpha))
-        print("Future Discount: {}".format(gamma))
-        print("Random Action Rate: {}".format(epsilon))
-        print("(Average reward per 100 episodes)")
-        print("------------------------------------")
+        print("Discount Rate: {}".format(gamma))
+        print("Initial Exploration Rate: {}".format(epsilon))
+        print("------------------------------")
         self.qtable = np.random.uniform(low = -1, high = 1, size = (19, 15, 3)) 
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
+        self.min_epsilon = min_epsilon
         self.env = gym.make('MountainCar-v0')
 
-    # Round state[0] to .1 and multiply by 10, round state[1] to .01 and multiply by 100
+    # multiply to make values whole numbers then round up
     def descretize_state(self, state):
-        position = int(round(state[0], 1) * 10)
-        velocity = int(round(state[1], 2) * 100)
+        position = math.ceil(state[0] * 10)
+        velocity = math.ceil(state[1] * 100)
         return [position, velocity]
+                       
  
     def train(self, episodes):
         ave_reward = 0
         last100sucessrate = 0
+        epsilon_reduction = (self.epsilon - self.min_epsilon)/episodes
+        
         for i in range(episodes):
 
             # Initialize params
@@ -51,11 +54,14 @@ class mountaincar_driver:
                 old_value = self.qtable[state[0], state[1], action]
                 nextmax = np.max(self.qtable[next_state[0], next_state[1]])
                 self.qtable[state[0], state[1], action] = (1-self.alpha)*old_value + self.alpha*(reward + self.gamma*nextmax)
-               
+
                 # update variables
                 total_reward += reward
                 state = next_state
 
+            # Decay Epsilon
+            if self.epsilon > self.min_epsilon:
+                self.epsilon -= epsilon_reduction
 
             # Keep track of average reward over 100 episodes
             ave_reward += total_reward
@@ -91,14 +97,10 @@ class mountaincar_driver:
 # Main
 alpha = .2 # learning rate
 gamma = .9 # future reward discount
-epsilon = .1 # random action rate
+epsilon = .8 # random action rate
+min_epsilon = 0
 episodes = 5000
 
-dEarnhardt = mountaincar_driver(alpha, gamma, epsilon)
+dEarnhardt = mountaincar_driver(alpha, gamma, epsilon, min_epsilon)
 dEarnhardt.train(episodes)
 dEarnhardt.display(3)
-
-
-
-
-
